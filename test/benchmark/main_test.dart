7af6c14c6b73fac75898ad8918bf027811350a85
Nike <nikechukwu@gmail.com>
2024-02-16 20:20:45 -0500

    Done writing up tests

--- test/benchmark/main_test.dart
+++ test/benchmark/main_test.dart
@@ -1,1 +1,35 @@
-void main() {}
+@Timeout(Duration(seconds: 1))
+
+import 'package:pheasant_temp/pheasant_temp.dart';
+import 'package:test/test.dart';
+
+
+void main() {
+  group('speed test', () {
+    List<String> results = List<String>.filled(5, '');
+
+    test('speed run', () {
+      final Stopwatch stopwatch = Stopwatch()..start();
+
+      results[0] = renderMain();
+      print('Result 1: ${stopwatch.elapsedMilliseconds}ms');
+      stopwatch.reset();
+
+      results[1] = renderMain(appName: "HomeComponent");
+      print('Result 2: ${stopwatch.elapsedMilliseconds}ms');
+      stopwatch.reset();
+
+      results[2] = renderMain(appName: "ThisIsAnExtremelyLongClassNameThatServesNoPracticalPurposeButIsUsedHereForDemonstrationPurposesComponent");
+      print('Result 3: ${stopwatch.elapsedMilliseconds}ms');
+      stopwatch.reset();
+
+      results[3] = renderMain(appName: "HomeComponent", mainEntry: "Home.phs");
+      print('Result 4: ${stopwatch.elapsedMilliseconds}ms');
+      stopwatch.reset();
+
+      results[4] = renderMain(appName: "ThisIsAnExtremelyLongClassNameThatServesNoPracticalPurposeButIsUsedHereForDemonstrationPurposesComponent", mainEntry: "ThisIsAnExtremelyLongClassNameThatServesNoPracticalPurposeButIsUsedHereForDemonstrationPurposes.phs");
+      print('Result 5: ${stopwatch.elapsedMilliseconds}ms');
+      stopwatch.stop();
+    });
+  });
+}
--- test/main/render_file_test.dart
+++ test/main/render_file_test.dart
@@ -71,8 +71,6 @@ void main() {
   ])'''));
     });
 
-    print(output);
-
 
     test('render function', () {
       assert(RegExp(r'_i2.Element ' + variableNameRegExp.pattern + r" = _i2\.Element\.tag\(\'div\'\)").hasMatch(output));
@@ -81,6 +79,7 @@ void main() {
       assert(RegExp(r'_i2.Element ' + variableNameRegExp.pattern + r" = _i2\.Element\.tag\(\'h2\'\)").allMatches(output).length >= 2);
       assert(RegExp(r'_i2.Element ' + variableNameRegExp.pattern + r" = _i2\.Element\.tag\(\'div\'\)").allMatches(output).length >= 2);
       assert(RegExp(r'_i2.Element ' + variableNameRegExp.pattern + r" = _i2\.Element\.tag\(\'a\'\)").allMatches(output).length >= 2);
+      expect(output, contains('for (int i = 0; i < 2; ++i) {'));
       assert(RegExp(r'\<code\>markdown\<\/code\>').hasMatch(output));
       assert(RegExp(r'_i2.StyleElement ' + styleVariableNameRegExp.pattern + r" = _i2\.StyleElement\(\)").hasMatch(output));
       assert(RegExp(styleVariableNameRegExp.pattern).hasMatch(output));
@@ -94,7 +93,7 @@ void main() {
 import 'src/Body.phs' as Body;
 import 'src/Header.phs' as Header;
 import 'src/Footer.phs' as Footer;
-import 'src/folder/Component.phs as Component;
+import 'src/folder/Component.phs' as Component;
 
 @prop
 int count;
@@ -115,13 +114,13 @@ bool isKnownFruit(String fruit) {
 <div>
   <Header>
     <h1>Hello, and Welcome to the Second Pheasant Test</h1>
-    <div p-if="count % 2 == 0">
+    <div p-if="count % 2 == 0" class="test">
       <p>Count is odd</p>
     </div>
   </Header>
-  <Body>
+  <Body class="test">
     <input @userFruit>
-    <button p-on:click="if (isKnownFruit(userFruit)) alert("Known Fruit");">
+    <button p-on:click="if (isKnownFruit(userFruit)) alert('Known Fruit');">
       Click This Button
     </button>
     <Component />
@@ -138,46 +137,96 @@ bool isKnownFruit(String fruit) {
 </div>
 ''';
   String style = '''
-  .test {
-    color: gold;
+\$font: 'Lato';
+\$monoFont: 'Roboto Mono';
+\$primary-color: red;
+
+.test {
+  color: rgb(245, 193, 66);
+  align-content: flex-start;
+  font-family: \$font, sans-serif;
+
+  button {
+      color: silver;
+      font-style: italic;
+      font-weight: 300;
   }
-''';
-  });
 
-  group('random case', () {
-    String script = '''
-import 'components/Component.phs' as Component;
+  p {
+      font-weight: 100;
+  }
 
-int myInt = 5;
-''';
-    String template = '''
-<div>
-  <h1>Hello, and Welcome to the first Pheasant Test</h1>
-  <h2>CASE I</h2>
-  <div p-for="int i = 0; i < 2; ++i">
-    <p>Test Subject</p>
-    <a preventDefault>Test Link</a>
-  </div>
-  <h2>CASE II</h2>
-  <md class="test">
-# CASE TWO
-This is the second component of this group, written in `markdown`.
-
-Markdown is a language very similar to html, and it even allows html to be written in it. 
-
-It also allows for code to be written in it
-```dart
-void main() {
-  print("Hello World")
+  code {
+      font-family: \$monoFont, monospace;
+  }
 }
-```
-  </md>
-</div>
 ''';
-  String style = '''
-  .test {
-    color: gold;
-  }
-''';
+
+    PheasantStyle testStyle = PheasantStyle.sassEnabled(data: style, syntax: 'scss');
+    test('prerequisite functions', () {
+      assert(testStyle.src == null);
+      assert(testStyle.syntax == 'scss');
+
+      PheasantScript testScript = PheasantScript(varDef: extractVariable(script), funDef: extractFunction(script), impDef: extractImports(script));
+
+      expect(testScript.fields.length, equals(3));
+      expect(testScript.imports.length, equals(4));
+      expect(testScript.nonjsMethods.length, equals(1));
+      expect(testScript.jsMethods.length, equals(1));
+      expect(testScript.props.length, equals(2));
+    });
+
+    String output = renderFunc(script: script, template: template, pheasantStyle: testStyle, sass: true);
+
+    test('main body', () {
+      expect(output, contains("import 'dart:html'"));
+      expect(output, contains("import 'package:pheasant/build.dart'"));
+      expect(output, contains("import 'src/folder/Component.phs.dart' as Component;"));
+      expect(output, contains("import 'src/Body.phs.dart' as Body;"));
+      expect(output, contains("import 'src/Footer.phs.dart' as Footer;"));
+      expect(output, contains("import 'src/Header.phs.dart' as Header;"));
+
+      expect(output, contains('@_i1.prop\n  int count;'));
+      expect(output, contains(
+'''  AppComponent({
+    super.template,
+    required this.count,
+    this.userFruit = "none specified",
+  });
+'''));
+      expect(output, contains('''@override
+  _i2.Element render(
+    String temp, [
+    _i1.TemplateState? state,
+  ])'''));
+    });
+
+    test('render function', () {
+      assert(RegExp(r'_i2.Element ' + variableNameRegExp.pattern + r" = _i2\.Element\.tag\(\'div\'\)").hasMatch(output));
+      assert(RegExp(variableNameRegExp.pattern + r'\.append\(_i2\.Text\(\"\"\"\n\"\"\"\)\)').hasMatch(output));
+      assert(RegExp(r'_i2.Element ' + variableNameRegExp.pattern + r" = _i2\.Element\.tag\(\'h1\'\)").allMatches(output).isNotEmpty);
+      assert(RegExp(r'_i2.Element ' + variableNameRegExp.pattern + r" = _i2\.Element\.tag\(\'input\'\)").allMatches(output).isNotEmpty);
+      assert(RegExp(r'_i2.Element ' + variableNameRegExp.pattern + r" = _i2\.Element\.tag\(\'button\'\)").allMatches(output).length >= 4);
+      assert(RegExp(variableNameRegExp.pattern + r'\.on\w+\.listen\(\(event\) \{').allMatches(output).length >= 4);
+      assert(RegExp(variableNameRegExp.pattern + r'\.onClick\.listen\(\(event\) \{').allMatches(output).length >= 4);
+      assert(RegExp(r'_i2.StyleElement ' + styleVariableNameRegExp.pattern + r" = _i2\.StyleElement\(\)").hasMatch(output));
+      assert(RegExp(styleVariableNameRegExp.pattern).hasMatch(output));
+      expect(output, contains('if (count % 2 == 0) {'));
+      expect(output, contains('event.preventDefault();'));
+      expect(output, contains('state?.freeze()'));
+      expect(output, contains('state?.unfreeze()'));
+      expect(output, contains('state?.reload()'));
+      expect(output, contains('''
+      if (!(state?.onPause ?? false)) {
+        if (isKnownFruit(userFruit)) alert('Known Fruit');
+        ;
+        state?.emit(event, templateState: this);
+      }
+'''));
+      assert(RegExp(variableNameRegExp.pattern + r'component = Component\.ComponentComponent\(\)').hasMatch(output));
+      assert(RegExp(variableNameRegExp.pattern + r'component = Body\.BodyComponent\(\)').hasMatch(output));
+      assert(RegExp(variableNameRegExp.pattern + r'component = Header\.HeaderComponent\(\)').hasMatch(output));
+      assert(RegExp(r'_i2.Element ' + variableNameRegExp.pattern + r' = ' + variableNameRegExp.pattern + r'component').hasMatch(output));
+    });
   });
 }
