003dceeeb38caec593d90d1f2cfe7e679b40ae72
Nike <nikechukwu@gmail.com>
2024-03-01 18:29:04 -0500

    Created Custom Components

--- lib/src/components/custom/custom.dart
+++ lib/src/components/custom/custom.dart
@@ -1,1 +1,27 @@
-// TODO: Implement Custom Components
+import 'dart:html';
+
+import 'package:pheasant_temp/src/base.dart';
+import 'package:pheasant_temp/src/state/state.dart';
+
+/// Base component for creating a custom component
+abstract class PheasantComponent extends PheasantTemplate {
+  PheasantComponent({required super.template});
+
+  @override
+  void init() {}
+
+  @override
+  void del() {}
+
+  Element renderComponent([TemplateState? state]);
+
+  Map<String, AttrFunc> attributes = {};
+
+  @override
+  Element render(String temp, [TemplateState? state]) {
+    return renderComponent(state);
+  }
+  
+}
+
+typedef AttrFunc = void Function(String value, Element target);
