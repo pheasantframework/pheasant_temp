57d7856faded9888f0424f67ef7bfec275e4d406
Nike <nikechukwu@gmail.com>
2024-03-06 13:01:45 -0500

    Added Preamble

--- lib/src/builder/code/constants/tempclass.dart
+++ lib/src/builder/code/constants/tempclass.dart
@@ -1,3 +1,11 @@
+// Copyright (c) 2024 The Pheasant Group. All Rights Reserved.
+// Please see the AUTHORS files for more information.
+// Intellectual property of third-party.
+// 
+// This file, as well as use of the code in it, is governed by an MIT License
+// that can be found in the LICENSE file.
+// You may not use this file except in compliance with the License.
+  
 /// A temporary class containing the new `beginningFunc` String [value] and the new `closebracket` integer [number].
 ///
 /// This class is not intended for public use and is only for one singular purpose - to get a return value for the `pheasantAttributes` function
--- lib/src/builder/code/src/events.dart
+++ lib/src/builder/code/src/events.dart
@@ -1,3 +1,11 @@
+// Copyright (c) 2024 The Pheasant Group. All Rights Reserved.
+// Please see the AUTHORS files for more information.
+// Intellectual property of third-party.
+// 
+// This file, as well as use of the code in it, is governed by an MIT License
+// that can be found in the LICENSE file.
+// You may not use this file except in compliance with the License.
+  
 import 'package:html/dom.dart' show Element;
 import 'package:pheasant_meta/pheasant_meta.dart' show From;
 import '../../../components/attributes/attr.dart'
