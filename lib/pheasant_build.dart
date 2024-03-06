d97b44571113f313efd3f5a6891f6ed15bf35cad
Nike <nikechukwu@gmail.com>
2024-03-06 13:01:02 -0500

    Adding Preamble

--- lib/pheasant_build.dart
+++ lib/pheasant_build.dart
@@ -1,3 +1,11 @@
+// Copyright (c) 2024 The Pheasant Group. All Rights Reserved.
+// Please see the AUTHORS files for more information.
+// Intellectual property of third-party.
+// 
+// This file, as well as use of the code in it, is governed by an MIT License
+// that can be found in the LICENSE file.
+// You may not use this file except in compliance with the License.
+  
 /// This library is not intended for use within this package, but is used by sibling packages like pheasant_build for the [createApp] function.
 /// This is also the library used in all compiled pheasant files cached during development.
 ///
